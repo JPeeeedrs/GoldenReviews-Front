a4068d868fa87453103bff00cd1926a45302952b
JPeeeedrs <joaopedrodaumas@gmail.com>
2026-06-07 21:13:28 -0300

    refactor/organizacao do codigo

--- frontend/src/games.jsx
+++ frontend/src/games.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import SteamSpyTopGames from "../topics_analizers/games_analizer/games_anlyses.jsx";
+import SteamSpyTopGames from "../../topics_analizers/games_analizer/games_anlyses.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
